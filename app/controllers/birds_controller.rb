38aa6fa4be2d07ddd903bd05c1a37e318704321e
Patrick Mason <patrickmason73@gmail.com>
2023-07-18 13:49:27 -0500

    commit tree e53b471645a0ee7bc53ada3df7b3230a96b2de8e

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -16,4 +16,10 @@ class BirdsController < ApplicationController
     end
   end
 
+  def create
+    bird = Bird.create(name: ???, species: ???)
+    render json: bird, status: :created
+  end
+
+  
 end
